22d43ee635980f2d41b3b56d921d89dd16a3bbc3
Allan Schmidt Hansen <ash@ipwsystems.dk>
2025-08-31 21:39:08 +0200

    Added directory and file creation for pages and posts

--- src/tomrummet/marker/src/Repository/ScaffoldRepository.php
+++ src/tomrummet/marker/src/Repository/ScaffoldRepository.php
@@ -2,16 +2,83 @@
 
 namespace Tomrummet\Marker\Repository;
 
+use Symfony\Component\Filesystem\Exception\IOException;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Tomrummet\Marker\Model\MarkerTypeEnum;
 
 class ScaffoldRepository
 {
     public function __construct(
         public MarkerRepository $markerRepository,
+        public SluggerInterface $slugger,
     ) {}
 
     public function getType(MarkerTypeEnum $type): string
     {
         return $type->folder();
+    }
+
+    public function getSlugByName(string $name): string
+    {
+        return strtolower($this->slugger->slug($name));
+    }
+
+    public function getPath(
+        string $name,
+        MarkerTypeEnum $type,
+    ): string {
+        $slug = $this->getSlugByName($name);
+        $path = $this->markerRepository->getContentDirectory($type->folder());
+
+        return $path . $slug;
+    }
+
+    public function createFolder(
+        string $name,
+        MarkerTypeEnum $type,
+    ): bool {
+        $path = $this->getPath(
+            name: $name,
+            type: $type,
+        );
+
+        try {
+            $filesystem = new Filesystem();
+            $filesystem->mkdir($path);
+
+            return true;
+        } catch (IOException $e) {
+            return false;
+        }
+    }
+
+    public function createFiles(
+        string $name,
+        MarkerTypeEnum $type,
+    ): bool {
+        $path = $this->getPath(
+            name: $name,
+            type: $type,
+        );
+
+        $files['content'] = $type === MarkerTypeEnum::PAGE
+            ? "{$name}.md"
+            : "content.md"
+        ;
+
+        if ($type === MarkerTypeEnum::POST) {
+            $files['metadata'] = 'metadata.yaml';
+        }
+
+        try {
+            $filesystem = new Filesystem();
+
+            array_walk($files, static fn ($file) => $filesystem->touch("{$path}/{$file}"));
+
+            return true;
+        } catch (IOException $e) {
+            return false;
+        }
     }
 }
--- tests/Service/ScaffoldTest.php
+++ tests/Service/ScaffoldTest.php
@@ -4,20 +4,145 @@ namespace App\Tests\Service;
 
 use PHPUnit\Framework\Attributes\Test;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Filesystem\Filesystem;
 use Tomrummet\Marker\Model\MarkerTypeEnum;
 use Tomrummet\Marker\Repository\ScaffoldRepository;
 
 class ScaffoldTest extends KernelTestCase
 {
+    private string $fixturesFolder = 'tests/Fixtures/';
+
     #[Test]
     public function getType(): void
     {
         self::bootKernel();
         $container = static::getContainer();
 
-        $ScaffoldRepository = $container->get(ScaffoldRepository::class);
+        $scaffoldRepository = $container->get(ScaffoldRepository::class);
 
-        $this->assertEquals('pages', $ScaffoldRepository->getType(MarkerTypeEnum::PAGE));
-        $this->assertEquals('posts', $ScaffoldRepository->getType(MarkerTypeEnum::POST));
+        $this->assertEquals('pages', $scaffoldRepository->getType(MarkerTypeEnum::PAGE));
+        $this->assertEquals('posts', $scaffoldRepository->getType(MarkerTypeEnum::POST));
+    }
+
+    #[Test]
+    public function getSlugByName(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+
+        $scaffoldRepository = $container->get(ScaffoldRepository::class);
+
+        $this->assertEquals('test-post-aeoa', $scaffoldRepository->getSlugByName('Test post ÆØÅ'));
+    }
+
+    #[Test]
+    public function createFolder(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+
+        $scaffoldRepository = $container->get(ScaffoldRepository::class);
+
+        $pageName = 'Scaffold test page';
+        $pageFolder = $scaffoldRepository->getPath(
+            name: $pageName,
+            type: MarkerTypeEnum::PAGE,
+        );
+
+        $this->assertEquals(
+            $this->getTestContentsDirectory() . 'pages/scaffold-test-page',
+            $pageFolder
+        );
+
+        $this->assertDirectoryDoesNotExist($pageFolder);
+        $this->assertTrue($scaffoldRepository->createFolder(
+            name: $pageName,
+            type: MarkerTypeEnum::PAGE,
+        ));
+        $this->assertDirectoryExists($pageFolder);
+
+        $postName = 'Scaffold test post';
+        $postFolder = $scaffoldRepository->getPath(
+            name: $postName,
+            type: MarkerTypeEnum::POST,
+        );
+
+        $this->assertEquals(
+            $this->getTestContentsDirectory() . 'posts/scaffold-test-post',
+            $postFolder,
+        );
+
+        $this->assertDirectoryDoesNotExist($postFolder);
+        $this->assertTrue($scaffoldRepository->createFolder(
+            name: $postName,
+            type: MarkerTypeEnum::POST,
+        ));
+        $this->assertDirectoryExists($postFolder);
+
+        $filesystem = new Filesystem();
+        $filesystem->remove($pageFolder);
+        $filesystem->remove($postFolder);
+
+        $this->assertDirectoryDoesNotExist($pageFolder);
+        $this->assertDirectoryDoesNotExist($postFolder);
+    }
+
+    #[Test]
+    public function createFiles(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+
+        $scaffoldRepository = $container->get(ScaffoldRepository::class);
+
+        $pageName = 'Scaffold test page';
+        $this->assertTrue($scaffoldRepository->createFolder(
+            name: $pageName,
+            type: MarkerTypeEnum::PAGE,
+        ));
+
+        $this->assertTrue($scaffoldRepository->createFiles(
+            name: $pageName,
+            type: MarkerTypeEnum::PAGE,
+        ));
+
+        $pageFolder = $scaffoldRepository->getPath(
+            name: $pageName,
+            type: MarkerTypeEnum::PAGE,
+        );
+        $this->assertFileExists("{$pageFolder}/{$pageName}.md");
+
+        $postName = 'Scaffold test post';
+        $this->assertTrue($scaffoldRepository->createFolder(
+            name: $postName,
+            type: MarkerTypeEnum::POST,
+        ));
+
+        $this->assertTrue($scaffoldRepository->createFiles(
+            name: $postName,
+            type: MarkerTypeEnum::POST,
+        ));
+
+        $postFolder = $scaffoldRepository->getPath(
+            name: $postName,
+            type: MarkerTypeEnum::POST,
+        );
+        $this->assertFileExists("{$postFolder}/content.md");
+        $this->assertFileExists("{$postFolder}/metadata.yaml");
+
+        $filesystem = new Filesystem();
+        $filesystem->remove($pageFolder);
+        $filesystem->remove($postFolder);
+
+        $this->assertDirectoryDoesNotExist($pageFolder);
+        $this->assertDirectoryDoesNotExist($postFolder);
+    }
+
+    private function getTestContentsDirectory(): string
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+
+        return $container->getParameter('kernel.project_dir') . '/' . $this->fixturesFolder;
     }
 }
